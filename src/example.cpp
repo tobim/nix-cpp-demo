b639374afbe6a2fcf8937597a37dd40801563c9b
Tobias Mayer <tobim@fastmail.fm>
2018-03-19 15:02:49 +0100

    remove commented code, better output

--- src/example.cpp
+++ src/example.cpp
@@ -33,43 +33,14 @@ struct C {
 using V = Variant<A, B, C>;
 using tobim::visit;
 
-#if 0
-template <typename T>
-class subtype {
-    template <typename R>
-    friend auto operator|(R& rng, subtype /*self*/) {
-        return rng | view::filter([](const typename R::value_type& v) {
-                   return vns::holds_alternative<T>(v);
-               }) |
-               view::transform([](const typename R::value_type& v) {
-                   return vns::get<T>(v);
-               });
-    }
-};
-
-template <typename T, typename U>
-auto subtype2 = view::filter([](const T& v) {
-                    return vns::holds_alternative<A>(v);
-                }) |
-                view::transform([](const T& v) { return vns::get<U>(v); });
-#endif
-
 int main() {
     std::vector<V> vs{{a}, {b}, {a}};
 
-    // auto as = vs | subtype<A>();
-    // // auto as = vs | subtype2<V, A>;
-
-    // for (auto a : as) {
-    //     std::cout << a.val;
-    // }
-    // std::cout << std::endl;
-
-    std::cout << "build: " << platform::compiler << '\n'
-              << "dependency: " << platform::link::compiler << '\n';
+    std::cout << "compiler_id of current translation: " << platform::compiler << '\n'
+              << "compiler_id of link target:         " << platform::link::compiler << '\n';
 
     std::for_each(vs.begin(), vs.end(),
-                    visit([](A i) { std::cout << i.val << std::endl; },
-                          [](B i) { std::cout << i.val << std::endl; },
-                          [](C i) { std::cout << i.val << std::endl; }));
+                  visit([](A i) { std::cout << i.val << std::endl; },
+                        [](B i) { std::cout << i.val << std::endl; },
+                        [](C i) { std::cout << i.val << std::endl; }));
 }
